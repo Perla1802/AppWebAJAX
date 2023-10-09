ad62b6f45e7e3b1b416b98359db3ac20a382a2b9
Perla <pyuritzy21@gmail.com>
2023-10-08 20:43:23 -0600

    Agregar archivos de proyecto.

---
+++ Controllers/JsonController.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Web;
+using System.Web.Mvc;
+using AppWebAJAX.Models;
+
+
+namespace AppWebAJAX.Controllers
+{
+    public class JsonController : Controller
+    {
+        // GET: Json
+        public ActionResult Index()
+
+        {
+                return View();
+        }
+
+        //Regresar Json
+
+        public JsonResult ObtenerConsultas()
+        {
+            List<ConsultaUno> consultaUno;
+            List<ConsultaDos> consultaDos;
+            //Cadena de conexion
+            using (AdventureWorks2012Entities db = new AdventureWorks2012Entities())
+            {
+                //Consulta de SQL a tipo LINQ
+                consultaUno = (from poh in db.PurchaseOrderHeader
+                               join pod in db.PurchaseOrderDetail on poh.PurchaseOrderID equals pod.PurchaseOrderID
+                               join sm in db.ShipMethod on poh.ShipMethodID equals sm.ShipMethodID
+                               where poh.VendorID == 1662
+                               select new ConsultaUno //consulta LINQ para crear objetos de esta clase
+                               {
+                                   OrderID = poh.PurchaseOrderID,
+                                   VendorID = poh.VendorID,
+                                   OrderDetailID = pod.PurchaseOrderDetailID,
+                                   ProductID = pod.ProductID,
+                                   UnitPrice = pod.UnitPrice,
+                                   StockedQty = (int)pod.StockedQty,
+                                   NombreEnvio = sm.Name,
+                                   ShipBase = sm.ShipBase
+                               }).ToList();
+
+                consultaDos = (from poh in db.PurchaseOrderHeaders
+                               join pod in db.PurchaseOrderDetails on poh.PurchaseOrderID equals pod.PurchaseOrderID
+                               join p in db.Products on pod.ProductID equals p.ProductID
+                               join v in db.Vendors on poh.VendorID equals v.BusinessEntityID
+                               where poh.VendorID >= 1568 && poh.VendorID <= 1572
+                               select new ConsultaDos
+                               {
+                                   VendorID = poh.VendorID,
+                                   VendorIdV = v.BusinessEntityID,
+                                   PurchaseOrderID = poh.PurchaseOrderID,
+                                   PodPurchaseOrderID = pod.PurchaseOrderID,
+                                   SubTotal = poh.SubTotal,
+                                   UnitPrice = pod.UnitPrice,
+                                   OrderQty = pod.OrderQty,
+                                   ProductID = pod.ProductID,
+                                   ProductName = p.Name,
+                                   ProductColor = p.Color,
+                                   VendorName = v.Name,
+                                   VendorAccountNumber = v.AccountNumber
+                               }).ToList();
+            }
+            //Vraiable donde se almacenan las dos List (Consultas)
+            var ConsultasViewModel = new ConsultasViewModel
+            {
+                ConsultaUno = consultaUno,
+                ConsultaDos = consultaDos
+            };
+            //Retorno de un JSON con las consultas antes hechas
+            return Json(ConsultasViewModel, JsonRequestBehavior.AllowGet); //Permite solicitudes get (Se vuelve asíncrono)
+
+
+        }    
+        
+        
+        
+    }
+
+}
---
+++ Models/ConsultaUno.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AppWebAJAX.Models
+{
+    //Clase para el almacenamiento de las listas
+    public class ConsultasViewModel
+    {
+        public List<ConsultaUno> ConsultaUno { get; set; }
+        public List<ConsultaDos> ConsultaDos { get; set; }
+    }
+
+    //clases para almacenar los resultados de la consulta
+    public class ConsultaUno
+    { 
+        public int OrderID { get; set; }
+        public int VendorID { get; set; }
+        public int OrderDetailID { get; set; }
+        public int ProductID { get; set; }
+        public decimal UnitPrice { get; set; }
+        public int StockedQty { get; set; }
+        public string NombreEnvio { get; set; }
+        public decimal ShipBase { get; set; }
+    }
+
+    public class ConsultaDos
+    {
+        public int VendorID { get; set; }
+        public int VendorIdV { get; set; }
+        public int PurchaseOrderID { get; set; }
+        public int PodPurchaseOrderID { get; set; }
+        public decimal SubTotal { get; set; }
+        public decimal UnitPrice { get; set; }
+        public short OrderQty { get; set; }
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+        public string ProductColor { get; set; }
+        public string VendorName { get; set; }
+        public string VendorAccountNumber { get; set; }
+    }
+
+}
